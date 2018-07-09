0f5445016377248e6093a33048606d6abd9a99dc
hayato.yasui <hayato.yasui@accenture.com>
2018-07-09 10:09:09 +0900

    Refactor

--- Common/Logic/Preprocess.py
+++ Common/Logic/Preprocess.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 class Preprocess:
 
     @staticmethod
-    def fetch_csv_data_and_convert_format_to_df(data_dir,file_names_li):
+    def fetch_csv_data_and_convert_format_to_df(data_dir, file_names_li):
         for idx, f in enumerate(file_names_li):
             if idx == 0:
                 df_src = pd.read_csv(data_dir + f, encoding='cp932')
@@ -53,13 +53,13 @@ class Preprocess:
         return output_csv_file_name
 
     @staticmethod
-    def replace_values(df,unexpected_val_dict,nan_val_dict):
-        [df[k].replace(v[0],v[1],inplace=True) for k, v in unexpected_val_dict.items()]
-        [df[k].fillna(v,inplace=True) for k,v in nan_val_dict.items()]
+    def replace_values(df, unexpected_val_dict, nan_val_dict):
+        [df[k].replace(v[0], v[1], inplace=True) for k, v in unexpected_val_dict.items()]
+        [df[k].fillna(v, inplace=True) for k, v in nan_val_dict.items()]
         return df
 
     @staticmethod
-    def convert_dtype(df,_dict):
+    def convert_dtype(df, _dict):
         # TODO Automation convert
         for k, v in _dict.items():
             df[k] = df[k].astype(v)
@@ -67,15 +67,16 @@ class Preprocess:
 
     @staticmethod
     def grouping(df, key_li, grouping_item_and_way_dict):
-        all_cols = key_li + [k for k,v in grouping_item_and_way_dict.items()]
-        df =df[all_cols]
+        all_cols = key_li + [k for k, v in grouping_item_and_way_dict.items()]
+        df = df[all_cols]
         df_grouped_src = df.groupby(key_li)
         df_grouped = df_grouped_src.agg(grouping_item_and_way_dict).reset_index()
         return df_grouped
 
     @staticmethod
-    def tanspose_cols_and_rows(df, keys_li, tgt_cols_li,tmp_cols):
+    def tanspose_cols_and_rows(df, keys_li, tgt_cols_li, tmp_cols):
         tmp_cols = tmp_cols + ['D.数量']
         df = df[tmp_cols]
-        df_pivot = df.pivot_table(index=keys_li, columns=tgt_cols_li, values='D.数量', aggfunc=sum).fillna(0).astype('int').reset_index()
+        df_pivot = df.pivot_table(index=keys_li, columns=tgt_cols_li, values='D.数量', aggfunc=sum).fillna(0).astype(
+            'int').reset_index()
         return df_pivot
